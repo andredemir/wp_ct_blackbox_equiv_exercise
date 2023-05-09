b100831b46d8f4fbaede80b459ee2346a33b10eb
I3ahsoxx <andre.demir@haw-hamburg.de>
2023-05-09 14:30:27 +0200

    neue Tests

--- tests/BlackBoxTest1Template .java
+++ tests/BlackBoxTest1Template .java
@@ -1,30 +1,280 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
-class BlackBoxTest2Template {
+class BlackBoxTest1Template {
 
 	@Test
-	void test() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 5;
-		int y = 1;
-		boolean b = false;
+	void testGAEx1_GAEy1() {
+		Color expected = Color.RED;
+		int x = -10;
+		int y = -1;
 		Color actual = BlackBox1.G(x, y);
 		assertEquals(expected, actual);
-		fail("not implemented yet");
 	}
 
-	
-	
+	@Test
+	void testGAEx1_GAEy2() {
+		Color expected = Color.YELLOW;
+		int x = -10;
+		int y = 4;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+		//zwingt zu failen
+		//fail("not implemented yet");
+	}
+
+	@Test
+	void testGAEx1_GAEy3() {
+		Color expected = Color.YELLOW;
+		int x = -10;
+		int y = 60;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAEx1_UGAEy1() {
+		Color expected = null;
+		int x = -10;
+		int y = -10;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAEx1_UGAEy2() {
+		Color expected = null;
+		int x = -10;
+		int y = 102;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx2xGAy1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 50;
+		int y = -1;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx2xGAy2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 50;
+		int y = 4;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx2xGAy3() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 50;
+		int y = 60;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx2xUAy1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 50;
+		int y = -5;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx2xUAy2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 50;
+		int y = 110;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx3xGAy1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 108;
+		int y = -1;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx3xGAy2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 108;
+		int y = 4;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx3xGAy3() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.GREEN;
+		int x = 108;
+		int y = 60;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testGAx3xUAy1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 108;
+		int y = -5;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void testGAx3xUAy2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 108;
+		int y = 110;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+
+	}
+
 	// ------- DO NOT TOUCH BELOW THIS LINE -------
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		assertTrue(BlackBox1.evalEqClasses(), "Not all equivalence classes covert!");
 	}
 
+	//-----------------------
+	@Test
+	void test_ux2_uy1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 300;
+		int y = -100;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void test_ux2_uy2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 300;
+		int y = 300;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void test_ux2_gy1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 300;
+		int y = -1;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void test_ux2_gy2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 300;
+		int y = 5;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void test_ux2_gy3() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 300;
+		int y = 50;
+
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+
+	}
+
+	//-----------------------
+	@Test
+	void ungueltigeAequivalenzklassex1_mit_UAey1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -30;
+		int y = -10;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void ungueltigeAequivalenzklassex1_mit_UAey2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -30;
+		int y = 110;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void ungueltigeAequivalenzklassex1_mit_GAey1() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -30;
+		int y = -1;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void ungueltigeAequivalenzklassex1_mit_GAey2() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -30;
+		int y = 5;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void ungueltigeAequivalenzklassex1_mit_GAey3() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -30;
+		int y = 50;
+		Color actual = BlackBox1.G(x, y);
+		assertEquals(expected, actual);
+	}
 }
--- tests/BlackBoxTest2Template.java
+++ tests/BlackBoxTest2Template.java
@@ -5,22 +5,384 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
-class BlackBoxTest1Template {
+class BlackBoxTest2Template {
+
+	/*
+	vier gültige Aquivalenzklassen
+		Aq1 -> x
+		Aq2 -> x
+		Aq3 -> y
+		Aq4 -> y
+	vier ungültige Aquivalenzklassen
+		uAq1 -> x
+		uAq2 -> x
+		uAq3 -> y
+		uAq4 -> y
+	boolische Fälle
+		t -> true
+		f -> false
+	 */
+
+	// ------- ungültige Testfälle, da x hinten außerhalb der Range ------
 
 	@Test
-	void test() {
-		/* Colors: YELLOW, GREEN, RED */ 
+	void uAq2uAq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = -5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq2uAq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = -5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq2Aq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = -1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq2Aq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = -1;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq2Aq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = 1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq2Aq4f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = 1;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq2uAq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = 5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq2uAq4f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 20;
+		int y = 5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	// ------- ungültige Testfälle, da y außerhalb der Range ------
+
+	@Test
+	void Aq1uAq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 5;
+		int y = -5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq1uAq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 5;
+		int y = -5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq1uAq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 5;
+		int y = 5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq1uAq4f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 5;
+		int y = 5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq2uAq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 15;
+		int y = -5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq2uAq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 15;
+		int y = -5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq2uAq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 15;
+		int y = 5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq2uAq4f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = 15;
+		int y = 5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	// ------- ungültige Testfälle, da x vorne außerhalb der Range ------
+
+	@Test
+	void uAq1uAq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = -5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq1uAq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = -5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq1Aq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = -1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq1Aq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = -1;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq1Aq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = 1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq1Aq4f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = 1;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq1uAq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = 5;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void uAq1uAq4f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = null;
+		int x = -5;
+		int y = 5;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	// ------- gültige Testfälle ------
+
+	@Test
+	void Aqu1Aq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 5;
+		int y = -1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq1Aq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 5;
+		int y = -1;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq1Aq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.RED;
+		int x = 5;
+		int y = 1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq1Aq4f() {
+		/* Colors: YELLOW, GREEN, RED */
 		Color expected = Color.YELLOW;
 		int x = 5;
 		int y = 1;
 		boolean b = false;
 		Color actual = BlackBox2.F(x, y, b);
 		assertEquals(expected, actual);
-		fail("not implemented yet");
 	}
 
-	
-	
+	@Test
+	void Aq2Aq3t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 15;
+		int y = -1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq2Aq3f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.GREEN;
+		int x = 15;
+		int y = -1;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq2Aq4t() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.YELLOW;
+		int x = 15;
+		int y = 1;
+		boolean b = true;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void Aq2Aq4f() {
+		/* Colors: YELLOW, GREEN, RED */
+		Color expected = Color.GREEN;
+		int x = 15;
+		int y = 1;
+		boolean b = false;
+		Color actual = BlackBox2.F(x, y, b);
+		assertEquals(expected, actual);
+	}
+
 	// ------- DO NOT TOUCH BELOW THIS LINE -------
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
